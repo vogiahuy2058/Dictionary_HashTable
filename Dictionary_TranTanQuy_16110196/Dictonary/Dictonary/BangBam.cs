ca048b1c62a25923b5c888503b419f0cb1711caf
= <=>
2018-10-07 16:12:47 +0700

    class Word & ham Bam, update ngay 7/10/2018

---
+++ Dictionary_TranTanQuy_16110196/Dictonary/Dictonary/BangBam.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictonary
+{
+    public class BangBam
+    {
+        public int Bam(char c)
+        {
+            if (c >= 'A' && c <= 'Z')       //nếu ký tự đầu là chữ in hoa từ A->Z, cộng 32 để chuyển
+                c = (char)((int)c + 32);    //sang chữ thường, sau đó ép kiểu int cho ký tự
+            return (int)c % 97;             //và hash với key là 97
+        }
+    }
+}
---
+++ Dictionary_TranTanQuy_16110196/Dictonary/Dictonary/Word.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictonary
+{
+    public class Word
+    {
+        private string name;
+        private string meaning;
+        public Word(string name, string meaning)
+        {
+            this.name = name;
+            this.meaning = meaning;
+        }
+        public string Name
+        {
+            set { name = value; }
+            get { return name; }
+        }
+        public string Meaning
+        {
+            set { meaning = value; }
+            get { return meaning; }
+        }
+    }
+}
