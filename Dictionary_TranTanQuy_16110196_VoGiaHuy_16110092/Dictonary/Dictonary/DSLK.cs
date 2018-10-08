9e993defd2baf2d79f58f6867e80c5c4440f73a0
= <=>
2018-10-08 22:12:34 +0700

    class Node, class DSLK, input.txt from Huy

---
+++ Dictionary_TranTanQuy_16110196_VoGiaHuy_16110092/Dictonary/Dictonary/DSLK.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictonary
+{
+    public class DSLK
+    {
+        public Node Head;
+        public Node Tail;
+
+        public DSLK()
+        {
+            Head = Tail = null;
+        }
+        public void AddHead(Node p)
+        {
+            if (Head == null)
+                Head = Tail = p;
+            else
+            {
+                p.Next(Head);
+                // Cach 2:
+                // p.pNext = Head;
+                Head = p;
+            }
+        }
+        public void AddTail(Node p)
+        {
+            if (Head == null)
+                Head = Tail = p;
+            else
+            {
+                Tail.Next(p);
+                Tail = p;
+            }
+        }
+        public void Duyet()
+        {
+            Node p = Head;
+            try
+            {
+                if (p == null)
+                    throw new Exception("Empty List");
+                while (p != null)
+                {
+                    Console.WriteLine("{0} {1}", p.word.Name, p.word.Meaning);
+                    p = p.pNext;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        public Node Search(string s)
+        {
+            Node p = Head;
+            while (p != null)
+            {
+                if (p.GetWord().Name == s)
+                    return p;
+                p = p.pNext;
+            }
+            return null;
+        }
+        public Node SearchTruoc(string s)
+        {
+            Node p = Head.pNext;
+            Node q = Head;
+            if (Head != Tail)
+            {
+                while (p != null)
+                {
+                    if (p.GetWord().Name == s)
+                        return q;
+                    p = p.pNext;
+                    q = q.pNext;
+                }
+            }
+            return null;
+        }
+        public void AddAfter(Node p, Node q)
+        {
+            try
+            {
+                if (Head == null || Head == Tail || p == null)
+                    throw new Exception("Can not Add");
+                q.Next(p.GetNext());
+                p.Next(q);
+                if (p == Tail)
+                    Tail = q;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        public void RemoveHead()
+        {
+            try
+            {
+                if (Head == null)
+                    throw new Exception("Empty List");
+                Node p = Head;
+                Head = p.GetNext();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        public void RemoveAfter(Node p)
+        {
+            try
+            {
+                if (p.pNext == null || Head == null || Head == Tail || p == null)
+                    throw new Exception("Can not Remove");
+                Node q = p.GetNext();
+                p.Next(q.GetNext());
+                if (p.GetNext() == null)
+                    Tail = p;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+    
+}
---
+++ Dictionary_TranTanQuy_16110196_VoGiaHuy_16110092/Dictonary/Dictonary/Node.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictonary
+{
+    public class Node
+    {
+        public Word word; 
+        public Node pNext; 
+
+        public Node()
+        {
+            word = new Word(null, null);
+            pNext = null;
+        }
+        public void CreateNode(Word wd)
+        {
+            word.Name = wd.Name;
+            word.Meaning = wd.Meaning;
+            pNext = null;
+        }
+        public void Next(Node x)
+        {
+            pNext = x; // ham thay doi pNext
+        }
+        public Word GetWord()
+        {
+            return word;
+        }
+        public Node GetNext()
+        {
+            return pNext;
+        }
+    }
+}
